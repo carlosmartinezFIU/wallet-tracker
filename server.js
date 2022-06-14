0def1698641cf95168054a1ee312e8ab40692ce7
carlosmartinezFIU <cmart566@fiu.edu>
2022-06-13 22:55:36 -0400

    updated port variable

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const pool = require('./db');
-const PORT = process.env.PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 
 const app = express();
